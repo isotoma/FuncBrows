4b4fa280b688e1e67582518d69304c34f4be9c31
Tom Wardill <tom@howrandom.net>
2010-04-23 13:57:11 +0100

    Added some unit tests

---
+++ test_funcbrows.py
@@ -0,0 +1,23 @@
+import unittest
+
+from funcbrows import FuncBrows
+
+class FuncTests(unittest.TestCase):
+    
+    def test_creation_testbrowser(self):
+        f = FuncBrows('testbrowser', 'http://www.google.com')
+        
+    def test_creation_selenium(self):
+        f = FuncBrows('*mock', 'http://www.google.com', host = '192.168.90.130', port = 4444)
+        
+        
+    def test_google_testbrowser(self):
+        f = FuncBrows('testbrowser', 'http://www.google.com')
+        f.open('/')
+        self.assertTrue('Google' in f.page_title)
+        
+        
+    def test_google_selenium(self):
+        f = FuncBrows('IE6', 'http://www.google.com', host = '192.168.90.130', port = 4444)
+        f.open('/')
+        self.assertTrue('Google' in f.page_title)

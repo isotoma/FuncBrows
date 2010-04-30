b96cf5056fd1e6072cf1e705331a022e2b94012c
Tom Wardill <tomwardill@RACK-1.(none)>
2010-04-30 21:30:10 +0100

    An attempt at making setup.py work. Currently not working

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = '0.1.2'
+
+setup(
+    name = 'FuncBrows',
+    version = version,
+    description = 'Web functional testing abstraction layer',
+    long_description = open("README.rst").read(),
+    url = "http://pypi.python.org/pypi/funcbrows",
+    classifiers = ["Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python"],
+    keywords = "functional test web browser",
+    author = "Tom Wardill",
+    author_email = "tom.wardill@isotoma.com",
+    license = "BSD License",
+    packages = find_packages(exclude=['ez_setup']),
+    package_dir = {'FuncBrows': 'FuncBrows'},
+    package_data = {
+        '':['README.rst'],
+        'FuncBrows': ['test.html']
+    },
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = [
+        'setuptools',
+        'zc.testbrowser'
+    ]
+)

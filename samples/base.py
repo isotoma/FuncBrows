cce10bbe31c6cf053203dd134e99cca3ceb212f9
Thomas Wardill <tomwardill@ubuntu.(none)>
2010-05-03 10:28:19 +0100

    Fixed samples

--- samples/base.py
+++ samples/base.py
@@ -6,12 +6,24 @@ import random
 
 class TestUsers(unittest.TestCase):
 
+    skip = False
+
+    def _configure_browser(self):
+        # the base class can't be run, so skip it if it's tried 
+        self.skip = True
+
     def setUp(self):
         self.browser = self._configure_browser()
 
     def tearDown(self):
+        if self.skip:
+            return
         self.browser.shutdown()
 
     def test_open(self):
-        browser.open('/')
+
+        if self.skip:
+            return
+
+        self.browser.open('/')
         self.assertTrue('Google' in self.browser.page_title)
--- samples/testbrower.py
+++ samples/testbrower.py
@@ -1,10 +1,8 @@
-from simple_portal_tests_base import TestUsers
+from base import TestUsers
 from FuncBrows import funcbrows
     
 class TestTestBrowser(TestUsers):
 
     def _configure_browser(self):
         return funcbrows.FuncBrows('testbrowser', 'http://www.google.com')
-    
-~                                                                                                                            
-~
+
